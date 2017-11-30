7536dd9cc182ac66d59ee2e797f59c7a2a8147b3
Hsuan-Hau (Howard) Liu <23202168+hsuanhauliu@users.noreply.github.com>
2017-11-29 19:32:45 -0600

    update

--- MIM.py
+++ MIM.py
@@ -5,30 +5,47 @@ def run() :
     MAXSIZE = 128
 
     # Initializing dictionaries
-    counter = -1
-    neighborhood = {}
-    while counter < MAXSIZE:
-        neighborhood.setdefault(counter, 0)
-
-    counter = 0
     allNotes = {}
-    while counter < MAXSIZE:
-        allNotes.setdefault(counter, neighborhood)
 
     # Import MIDI files
     pattern = midi.read_midifile("melody1.mid")
     track = pattern[0]
 
     # Grab information for each note
+    prevP = -1   
+
     for event in track:
-        if event.name == 'Note On':
-            tick = event.tick
-            pitch = event.data[0]
-            volume = event.data[1]
-            if volume == 0:
-                print "Tick={}, Note={}, Volume={}".format(tick, pitch, volume)
+        if event.name == 'Note On' and event.data[1] == 0:
+            if prevP != -1:
+                # get data of current note
+                tick = event.tick
+                currP = event.data[0]
+                
+                # if the note exists
+                if allNotes.has_key(prevP):
+                    # if the neighbor exists
+                    if allNotes[prevP].has_key(currP):
+                        allNotes[prevP][currP] += 1
+                    # if not
+                    else:
+                        allNotes[prevP][currP] = 1
+                    allNotes[prevP][-1] += 1
+                # if not
+                else:
+                    allNotes[prevP] = {-1: 1, currP: 1}
+                
+                # set previous pitch to current pitch
+                prevP = currP
+                #print "Tick={}, Note={}".format(tick, currP)
+            else:
+                prevP = event.data[0]
 
-
+    # print out all the neighbors
+    for note in allNotes:
+        print "Note", note, "-> ",
+        for neighbor in allNotes[note]:
+            print neighbor, ":", allNotes[note][neighbor], ",",
+        print ""
 
 
     # Process
@@ -44,9 +61,11 @@ def run() :
 
 
     # Export MIDI file.
-    midi.write_midifile("aiSong.mid", pattern)
+
 
 
 
 
     return
+
+run()
