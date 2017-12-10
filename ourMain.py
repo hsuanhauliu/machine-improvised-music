09996a61342bf26c638130e211657f5202cdaefc
lakumiK <lakumiK@users.noreply.github.com>
2017-12-09 18:57:10 -0600

    Update main
    Tests all approaches, generating separate .mid files for each approach.

--- ourMain.py
+++ ourMain.py
@@ -8,11 +8,11 @@ import MIM_uniqueApproach
 allNotes = {} # Dictionary for pitches
 allLengths = [] # List for note lengths
 
-files = MIM_uniqueApproach.getMusic()
-avgLength = MIM_uniqueApproach.listen(files, allNotes, allLengths)
-allLengths = sorted(allLengths)
-MIM_uniqueApproach.printInfo(allNotes, allLengths)
-newSong = MIM_uniqueApproach.createMusic(allNotes, allLengths, avgLength)
-
-# Export MIDI file.
-midi.write_midifile("aiSong.mid", newSong)
+files = MIM_uniqueApproach.getMusic() # Grab input files
+avgLength = MIM_uniqueApproach.listen(files, allNotes, allLengths) # Learn statistical data from input songs
+allLengths = sorted(allLengths) # Sort length list from shortest to longest note lengths
+MIM_uniqueApproach.printInfo(allNotes, allLengths) # Print statistical information about notes and lengths
+greedySong = MIM_uniqueApproach.createGreedyMusic(allNotes, allLengths, avgLength) # Generate song using greedy approach
+fixedProbSong = MIM_uniqueApproach.createFixedProbabilityMusic(allNotes, allLengths, avgLength) # Generate song using fixed probability approach
+dynamicProbSong = MIM_uniqueApproach.createDynamicProbabilityMusic(allNotes, allLengths, avgLength) # Generate song using calculated probability approach
+optimalSong = MIM_uniqueApproach.createMusic(allNotes, allLengths, avgLength) # Generate song using calculated probability and varied note lengths (with printed display of output)
